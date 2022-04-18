b39a9106bb5be96b15547656ee8977d6a6c65e43
JuniorNovoa1 <87715171+JuniorNovoa1@users.noreply.github.com>
2022-04-18 09:32:34 -0400

    Examples (For people who's brains are goldfish) (#31)
    * EXAMPLE
    
    * example
    
    * why tf is this like that
    
    * Update GJKeys.hx
    
    Co-authored-by: Tenta <72235309+TentaRJ@users.noreply.github.com>

---
+++ source/EXAMPLES/TitleState.hx
@@ -0,0 +1,14 @@
+package;
+
+import GameJolt.GameJoltAPI; //important
+import GameJolt; //important 
+
+class TitleState extends MusicBeatState
+{
+	override public function create():Void //this is where you wanna start gamejolt
+	{
+		//gamejolt start shit
+		GameJoltAPI.connect();
+                GameJoltAPI.authDaUser(FlxG.save.data.gjUser, FlxG.save.data.gjToken);
+	}	
+}
