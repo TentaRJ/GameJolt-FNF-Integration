5dc7fbcbf76825b10497e0146476490909c42f5c
Tenta <72235309+TentaRJ@users.noreply.github.com>
2022-04-18 09:31:07 -0400

    Update GJKeys.hx

--- source/GJKeys.hx
+++ source/GJKeys.hx
@@ -3,5 +3,5 @@ package;
 class GJKeys
 {
     public static var id:Int = 	0; // Put your game's ID here
-    public static var key:String = ""; // Put your game's private API key here
+    public static var key:String = ""; // Put your game's private API key here, you can find it in your game's settings in GameJolt under the API tab!
 }
